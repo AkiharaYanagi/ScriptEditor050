df8b154c6ce23ea0c866c00a9102c44c05411bf9
AkiharaYanagi <150650129+AkiharaYanagi@users.noreply.github.com>
2025-08-24 17:05:04 +0900

    プロジェクト ファイルを追加します。

---
+++ ScriptEditor050/Form1.cs
@@ -0,0 +1,14 @@
+using ScriptEditor;
+
+
+namespace ScriptEditor050
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            FormUtility.InitPosition(this);
+            InitializeComponent();
+        }
+    }
+}
